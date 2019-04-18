5899fe2d69558307db35bf89ae08502134719bc0
Kara Bayse <karabayse@gmail.com>
2019-04-17 22:07:48 -0500

    plural function

--- edabit.js
+++ edabit.js
@@ -35,3 +35,10 @@ concatName(str2, str1);
 function remainder(x, y) {
 	return x % y;
 }
+
+
+// Create a function that takes in a word and determines whether or not it is plural.
+// A plural word is one that ends in "s".
+function isPlural(word) {
+
+}
