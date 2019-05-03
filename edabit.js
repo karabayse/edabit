a3bd2e3915ed87610d91016b158a2725122768a8
Kara Bayse <karabayse@gmail.com>
2019-05-03 16:32:27 -0500

    smaller number function

--- edabit.js
+++ edabit.js
@@ -159,5 +159,18 @@ ctoa("F");
 // If both numbers tie, return either number.
 // Numbers will be positive.
 function smallerNum(n1, n2) {
+  let num1 = parseInt(n1);
+  let num2 = parseInt(n2);
 
+  if (num1 > num2) {
+    console.log(num1);
+  } else if (num2 > num1) {
+    console.log(num2);
+  } else if (num1 == num2) {
+    console.log(num1 || num2);
+  }
 }
+
+smallerNum("2", "2");
+smallerNum("2", "1");
+smallerNum("4", "5");
