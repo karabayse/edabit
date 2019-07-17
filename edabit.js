132964a43a40952709770ab785c753e687d7ee5a
Kara Bayse <karabayse@gmail.com>
2019-07-17 18:08:59 -0500

    extract properties from an object

--- edabit.js
+++ edabit.js
@@ -701,8 +701,30 @@ function difference(nums) {
   let largest_number = Math.max(...nums);
 
   let diff = (largest_number - smallest_number);
-
   console.log(diff);
 }
 
 difference([50, 15, 3, 7, 24]);
+
+
+// Create a function that takes an object as an argument and
+// returns a string with facts about the city.
+// The city facts will need to be extracted from the object's three properties:
+// name
+// population
+// continent
+// The string should have the following format:
+// X has a population of Y and is situated in Z
+// (where X is the city name, Y is the population and
+// Z is the continent the city is situated in).
+function cityFacts(city) {
+
+}
+
+city(
+  cityFacts({
+    name: "Paris",
+    population: "2,140,526",
+    continent: "Europe"
+  }) ➞ "Paris has a population of 2,140,526 and is situated in Europe"
+)
