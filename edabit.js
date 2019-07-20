43bb728102dfc1145b4b6713b66fc07b5b865587
Kara Bayse <karabayse@gmail.com>
2019-07-20 13:59:20 -0500

    calculate the determinant of a matrix

--- edabit.js
+++ edabit.js
@@ -739,5 +739,8 @@ cityFacts({
 // Create a function to calculate the determinant of a 2 x 2 matrix
 // The determinant of the following matrix is: ad - bc: [[a, b], [c, d]]
 function calcDeterminant(matrix) {
-
+  let det = ((a * d) - (b * c));
+  console.log(det);
 }
+
+calcDeterminant([[2, 3], [4, 5]]);
