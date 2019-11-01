3ce6a1cfdceac6883bc3ee61d1df8f34fdd2f82e
Kara Bayse <karabayse@gmail.com>
2019-10-31 19:08:43 -0500

    find the bug

--- edabit.js
+++ edabit.js
@@ -1040,3 +1040,31 @@ function convert(hours, minutes) {
 }
 
 convert(4, 60);
+
+
+// Find the bug
+// The packaging machine is running the getContainer function to retrieve the
+// container of a product. But something is not right
+function getContainer(product) {
+	let container
+  switch (product) {
+		case "Bread":
+			container = "bag"
+		case "Beer":
+		case "Milk":
+			container = "bottle"
+			break
+		case "Cerials":
+			container = "box"
+			break
+		case "Eggs":
+			container = "carton"
+			break
+		case "Candy":
+			container = "plastic"
+		default:
+			container = null
+	}
+
+	return container
+}
