87cf05f3b2c387f50f0d620e259f38ee405cf486
Kara Bayse <karabayse@gmail.com>
2019-11-25 18:54:41 -0600

    determine leap years

--- edabit.js
+++ edabit.js
@@ -1190,5 +1190,11 @@ removeVowels("onomatopoeia");
 // Given a year you must implement a function that returns true if it's a leap
 // year, or false if it's not
 function isLeap(year) {
-
+  if (year % 400 === 0) {
+    console.log("Leap Year");
+  } else if (year % 4 === 0 && year % 100 !== 0) {
+    console.log("Leap Year");
+  } else {
+    console.log("Not a Leap Year");
+  }
 }
