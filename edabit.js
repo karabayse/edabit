aaca15682e79b2b6eb40edf4718c489ec3697bef
Kara Bayse <karabayse@gmail.com>
2019-11-30 15:51:23 -0600

    reverse coding challenge

--- edabit.js
+++ edabit.js
@@ -1222,6 +1222,24 @@ str("st");
 // satisfy the relationship between the inputs and outputs.
 // Your task is to create a function that, when fed the inputs below, produce
 // the sample outputs shown.
-function mysteryFunc(arr, num) {
+/*
+[5, 7, 8, 2, 1], 2 ➞ [1, 1, 0, 0, 1]  arr % num ?
 
+[9, 8, 16, 47], 4 ➞ [1, 0, 0, 3]
+
+[17, 11, 99, 55, 23, 1], 5 ➞ [2, 1, 4, 0, 3, 1]
+
+[6, 1], 7 ➞ [6, 1]
+
+[3, 2, 9], 3 ➞ [0, 2, 0]
+
+[48, 22, 0, 19, 33, 100], 10 ➞ [8, 2, 0, 9, 3, 0]
+*/
+function mysteryFunc(arr, num) {
+    let newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+    let newNum = arr[i] % num;
+		newArr.push(newNum);
+  }
+  return newArr;
 }
