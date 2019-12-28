fcaba2395a780db7ecaea2bc3aea5d0261b4d3ba
Kara Bayse <karabayse@gmail.com>
2019-12-28 10:53:07 -0600

    a strange pair of strings

--- edabit.js
+++ edabit.js
@@ -1311,3 +1311,13 @@ function MultiplyByLength(arr) {
   }
 	return newArr;
 }
+
+
+// A pair of strings form a strange pair if both of the following are true:
+// The 1st string's first letter = 2nd string's last letter.
+// The 1st string's last letter = 2nd string's first letter.
+// Create a function that returns true if a pair of strings constitutes a
+// strange pair, and false otherwise.
+function isStrangePair(str1, str2) {
+
+}
