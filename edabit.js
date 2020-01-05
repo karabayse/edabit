a31bb60b734d5333c0b2a3040611e4952fcc640c
Kara Bayse <karabayse@gmail.com>
2020-01-05 14:16:36 -0600

    exists a number higher

--- edabit.js
+++ edabit.js
@@ -1357,10 +1357,10 @@ function filterStateNames(arr, type) {
 
 // Write a function that returns true if there exists at least one number that
 // is larger than or equal to n.
-// Return false for an empty array. 
+// Return false for an empty array.
 function existsHigher(arr, n) {
-  for (var i = 0; i < arr.length; i++) {
-    if (n >= arr[i]) {
+  for (var i = 0; i <= arr.length; i++) {
+    if (n <= arr[i]) {
       return true;
     } else if (arr == null){
       return false;
