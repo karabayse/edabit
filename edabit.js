aefed45a53cb8fd35e7326cf87294e83f842d65f
Kara Bayse <karabayse@gmail.com>
2020-01-16 18:42:08 -0600

    odd up, even down

--- edabit.js
+++ edabit.js
@@ -1405,9 +1405,9 @@ function noOdds(arr) {
 function transform(arr) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] % 2 == 0) {
-      arr[i] += 1;
-    } else if (arr[i] % 2 != 0) {
       arr[i] -= 1;
+    } else if (arr[i] % 2 != 0) {
+      arr[i] += 1;
     }
   }
   return arr;
