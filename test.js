d2541ca73abd6d8cb13ab3b3b9a34c4b82c95ac8
Kara Bayse <karabayse@gmail.com>
2020-03-14 16:59:06 -0500

    lexicographical rearrangement

--- test.js
+++ test.js
@@ -10,23 +10,12 @@ function firstAndLast(s) {
   let sortArr = arr.sort(); // sort array in ascending order
   console.log(sortArr); // ["c", "o", "o", "p", "s"]
 
-  let firstLetter = sortArr[0].toString(); // get first letter of array
-  console.log(firstLetter); // returns c rather than s
-  console.log(typeof firstLetter); // returns string
-  let lastLetter = sortArr.slice(-1)[0].toString(); // get last letter of array
-  console.log(lastLetter); // returns s
+  let first = sortArr.toString(); // convert array to string
+  console.log(first); // c,o,o,p,s
+  let last = sortArr.reverse().toString(); // reverse array -> last
+  console.log(last); // s,p,o,o,c
 
-  let newStr = sortArr.toString();
-  console.log(newStr); // c,o,o,p,s
-  let removeFirst = newStr.substring(1); // remove first letter of string
-  console.log(removeFirst); // ,o,o,p,s
-  let first = removeFirst.concat(firstLetter);
-  console.log(first); // ,o,o,p,sc
-
-  let reverseStr = removeFirst.split(' ').reverse().join(' ');
-  console.log(reverseStr); // ,o,o,p,s
-  let last = lastLetter.concat(reverseStr);
-  console.log(last); // s,o,o,p,s
+  
   console.log(flArr.push(first, last)); // 2
 }
 
