5fbb15808653ca3cd3045a29ac8a54350546f57f
Kara Bayse <karabayse@gmail.com>
2020-03-14 15:53:15 -0500

    lexicographical rearrangement

--- edabit.js
+++ edabit.js
@@ -1675,11 +1675,13 @@ function firstAndLast(s) {
   let flArr = [];
   let arr = s.split(); // split string into array of letters
   let sortArr = arr.sort(); // sort array in ascending order
-  let joinArr = sortArr.join(); // convert string to array 
-  let firstLetter = joinArr[0]; // get first letter of array
-  let lastLetter = sortArr.slice(-1); // get last letter of string
-  let removeFirst = s.substring(1); // remove first letter of string
-  let first = removeFirst + lastLetter;
+
+  let firstLetter = sortArr[0]; // get first letter of array
+  let lastLetter = sortArr[-1]; // get last letter of array
+
+  let newStr = sortArr.toString();
+  let removeFirst = newStr.substring(1); // remove first letter of string
+  let first = removeFirst + firstLetter;
 
   let reverseStr = removeFirst.split("").reverse().join("");;
   let last = lastLetter + reverseStr;
