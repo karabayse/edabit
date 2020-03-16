fd2361e50e5dead9a61832fb5f9dfc94bc9d6ec3
Kara Bayse <karabayse@gmail.com>
2020-03-16 18:14:45 -0500

    the full length of a Google

--- test.js
+++ test.js
@@ -3,5 +3,22 @@
 // Create a function where given a number of pages n, return the word 'Google'
 // but with the correct number of "o"s.
 function googlify(n) {
+  let googStr = "Google";
+  let googArr = googStr.split('');
+  console.log(googArr); // ["G", "o", "o", "g", "l", "e"]
 
+  let oS = "o";
+
+  let googO = googArr.splice(2, 0, oS.repeat(n));
+  console.log(googO); // []
+
+  let google = googO.toString().replace(/,/g, '');
+  console.log(google); // 
 }
+
+googlify(8);
+
+/*
+const months = ['Jan', 'March', 'April', 'June'];
+months.splice(1, 0, 'Feb');
+*/
