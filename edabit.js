58512cfbdf39e7e7bc88f512da143e580fc1a112
Kara Bayse <karabayse@gmail.com>
2020-03-18 15:08:15 -0500

    check for identical characters

--- edabit.js
+++ edabit.js
@@ -1721,5 +1721,11 @@ googlify(8);
 // Write a function that returns true if all characters in a string are identical
 // and false otherwise.
 function isIdentical(s) {
-
+  if (s.split('').every(char => char === s[0])) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+isIdentical("identical");
--- test.js
+++ test.js
@@ -1,24 +1,10 @@
 // Write a function that returns true if all characters in a string are identical
 // and false otherwise.
 function isIdentical(s) {
-  let sArr = s.split('');
-  console.log(sArr);
-
-  let yesArr = [];
-  let noArr = [];
-
-  for (var i = 0; i < sArr.length; i++) {
-     if (sArr[i] == i) {
-       yesArr.push(sArr[i]);
-     } else {
-       noArr.push(sArr[i]);
-     }
-  }
-  console.log(yesArr);
-  console.log(noArr);
-
-  if (yesArr) {
-    console.log("True");
+  if (s.split('').every(char => char === s[0])) {
+    return true;
+  } else {
+    return false;
   }
 }
 
