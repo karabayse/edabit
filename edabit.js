94c460e1d3169ce1ba6b41739ceba55e88076791
Kara Bayse <karabayse@gmail.com>
2020-03-20 16:59:14 -0500

    check string ending

--- edabit.js
+++ edabit.js
@@ -1734,5 +1734,9 @@ isIdentical("identical");
 // Create a function that takes two strings and returns true if the first string
 // ends with the second string; otherwise return false.
 function checkEnding(str1, str2) {
-
+  if (str1.endsWith(str2)) {
+    return true;
+  } else {
+    return false;
+  }
 }
--- test.js
+++ test.js
@@ -1,5 +1,1 @@
-// Create a function that takes two strings and returns true if the first string
-// ends with the second string; otherwise return false.
-function checkEnding(str1, str2) {
 
-}
