fa587be27a17b55c46f028578a1065085ac16ffa
Kara Bayse <karabayse@gmail.com>
2020-03-24 17:27:44 -0500

    even numbers from 1

--- edabit.js
+++ edabit.js
@@ -1743,6 +1743,16 @@ function checkEnding(str1, str2) {
 
 
 // Create a function that finds all even numbers from 1 to the given number.
+// findEvenNums(8) ➞ [2, 4, 6, 8]
 function findEvenNums(num) {
+  let evenArr = [];
 
+  for (let i = 1; i <= num; i++) {
+    if (i % 2 == 0) {
+        evenArr.push(i);
+    }
+  }
+  console.log(evenArr);;
 }
+
+findEvenNums(8);
--- test.js
+++ test.js
@@ -1,4 +1,20 @@
 // Create a function that finds all even numbers from 1 to the given number.
-function findEvenNums(num) {
+/*
 
+
+findEvenNums(4) ➞ [2, 4]
+
+findEvenNums(2) ➞ [2]
+*/
+function findEvenNums(num) {
+  let evenArr = [];
+
+  for (let i = 1; i <= num; i++) {
+    if (i % 2 == 0) {
+        evenArr.push(i);
+    }
+  }
+  console.log(evenArr);;
 }
+
+findEvenNums(8);
