42bf8a906f6c2ae7daa51da53d769f0b7b7453b0
Kara Bayse <karabayse@gmail.com>
2020-03-29 19:00:54 -0500

    determine the winning string

--- test.js
+++ test.js
@@ -23,10 +23,15 @@ function showdown(p1, p2) {
   let arr1 = p1.split('');
   let arr2 = p2.split('');
 
-  // loop through array and determine where in the array the first value exists
+  // loop through arr1 and determine where in the array the first value exists
   for (var i = 0; i < arr1.length; i++) {
     arr1[i]
   }
 
-  // compare where first value in arr1 and arr2 exists 
+  // loop through arr2 and determine where in the array the first value exists
+  for (var i = 0; i < arr2.length; i++) {
+    arr2[i]
+  }
+
+  // compare where first value in arr1 and arr2 exists
 }
