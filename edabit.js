03f3d88695a62a688ca34e7911e41d6609a8126a
Kara Bayse <karabayse@gmail.com>
2020-04-07 20:17:53 -0500

    repeat the same item multiple times

--- edabit.js
+++ edabit.js
@@ -1819,6 +1819,9 @@ function isRepdigit(num) {
 // Create a function that takes two arguments (item, times). The first argument
 // (item) is the item that needs repeating while the second argument (times) is
 // the number of times the item is to be repeated. Return the result in an array.
+// item can be either a string or a number.
+// times will always be a number.
+// repeat("edabit", 3) ➞ ["edabit", "edabit", "edabit"]
 function repeat(item, times) {
 
 }
--- test.js
+++ test.js
@@ -1,39 +1,9 @@
-// Given two strings,p1 and p2, return which person drew their gun the fastest.
-// If both are drawn at the same time, return "tie".
-/*
-showdown(
-  "   Bang!        ",
-  "        Bang!   "
-) ➞ "p1"
+// Create a function that takes two arguments (item, times). The first argument
+// (item) is the item that needs repeating while the second argument (times) is
+// the number of times the item is to be repeated. Return the result in an array.
+// item can be either a string or a number.
+// times will always be a number.
+// repeat("edabit", 3) ➞ ["edabit", "edabit", "edabit"]
+function repeat(item, times) {
 
- // p1 draws his gun sooner than p2
-
-showdown(
-  "               Bang! ",
-  "             Bang!   "
-) ➞ "p2"
-
-showdown(
-  "     Bang!   ",
-  "     Bang!   "
-) ➞ "tie"
-*/
-
-function showdown(p1, p2) {
-  /*
-  localeCompare
-  str1.localeCompare(str2);
-  Returns -1 if str1 is sorted before str2
-  Returns 0 if the two strings are equal
-  Returns 1 if str1 is sorted after str2
-  */
-  if (p1.localeCompare(p2) == 1) {
-    console.log("p1");
-  } else if (p1.localeCompare(p2) == -1) {
-    console.log("p2");
-  } else if (p1.localeCompare(p2) == 0) {
-    console.log("tie");
-  }
 }
-
-showdown(" Bang", "Bang");
