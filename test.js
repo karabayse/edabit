dd7662c85720e56c7f21d879c7c4f51fc88012fc
Kara Bayse <karabayse@gmail.com>
2020-04-14 19:39:27 -0500

    repeat the same item multiple times

--- test.js
+++ test.js
@@ -6,8 +6,10 @@
 // repeat("edabit", 3) ➞ ["edabit", "edabit", "edabit"]
 function repeat(item, times) {
   let repArr = [];
+  let itemConcat = item+','+"";
+  console.log(itemConcat);
 
-  let repItem = item.repeat(times);
+  let repItem = itemConcat.repeat(times);
 
   repArr.push(repItem);
   console.log(repArr);  // ["edabitedabitedabit"]
