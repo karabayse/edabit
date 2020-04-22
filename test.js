ff1cc9a8579c7b74a29b1e6b3465efaa1e3d5a9f
Kara Bayse <karabayse@gmail.com>
2020-04-22 17:27:19 -0500

    is sam with frodo?

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-// Sam and Frodo need to be close. If they are side by side in the array, your
-// function should return true. If there is a name between them, return false.
-// middleEarth(["Frodo", "Sam", "Gandalf"]) ➞ true
+/*
+Sam and Frodo need to be close. If they are side by side in the array, your
+function should return true. If there is a name between them, return false.
+No matter who comes first, the result must be true if Frodo and Sam are side by side.
+There is only one Sam and one Frodo in the array.
+middleEarth(["Frodo", "Sam", "Gandalf"]) ➞ true
+*/
 function middleEarth(arr) {
 
 }
