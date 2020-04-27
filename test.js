b3ee821b0adf821020f35c3c7d0a386b2a553fb6
Kara Bayse <karabayse@gmail.com>
2020-04-26 20:53:39 -0500

    exists a number higher

--- test.js
+++ test.js
@@ -2,25 +2,14 @@
 // is larger than or equal to n.
 // Return false for an empty array.
 function existsHigher(arr, n) {
-  let trueArr = [];
-  let falseArr = [];
-  let maxNum = Math.max(arr);
-
-  for (var i = 0; i < arr.length; i++) {
-    if (n <= arr[i]) {
-      trueArr.push(arr[i]);
-      console.log(trueArr);
-    } else if (n > arr[i]) {
-      falseArr.push(arr[i]);
-      console.log(falseArr);
-    }
-    if (trueArr) {
-      console.log(true);
-    } else if (!Array.isArray(arr) || !arr.length) {
-      console.log(false);
-    }
-  }
+  arr.some(function (n) {
+	   if (item > n) {
+       console.log(true);
+     } else if (arr == []) {
+       console.log(false);
+     }
+   });
 }
 
-// existsHigher([1, 3, 4, 5], 6);
+existsHigher([1, 3, 4, 5], 6);
 existsHigher([], 7);
