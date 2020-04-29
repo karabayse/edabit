846dd39f34135893a9123994e47a8a722165b080
Kara Bayse <karabayse@gmail.com>
2020-04-29 18:22:53 -0500

    say hello to guests

--- edabit.js
+++ edabit.js
@@ -1893,7 +1893,14 @@ Take a list of names.
 Add "Hello" to every name.
 Make one big string with all greetings.
 The solution should be one string with a comma in between every "Hello (Name)".
+greetPeople(["Angela", "Joe"]) ➞ "Hello Angela, Hello Joe"
 */
 function greetPeople(names) {
+  names.forEach((name, index) => names[index] = `Hello ${name}`);
+  console.log(names);
 
+  let str = names.join(', ');
+  console.log(str);
 }
+
+greetPeople(["Ironman", "Superman", "Antman", "Aquaman"]);
--- test.js
+++ test.js
@@ -3,13 +3,14 @@ Take a list of names.
 Add "Hello" to every name.
 Make one big string with all greetings.
 The solution should be one string with a comma in between every "Hello (Name)".
+greetPeople(["Angela", "Joe"]) ➞ "Hello Angela, Hello Joe"
 */
 function greetPeople(names) {
   names.forEach((name, index) => names[index] = `Hello ${name}`);
   console.log(names);
 
-  names.toString();
-  console.log(names);
+  let str = names.join(', ');
+  console.log(str);
 }
 
 greetPeople(["Ironman", "Superman", "Antman", "Aquaman"]);
