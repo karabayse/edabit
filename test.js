11ad3f56df425b25ea1ff1fa5c2768d3f20be52c
Kara Bayse <karabayse@gmail.com>
2020-04-29 17:58:45 -0500

    say hello to guests

--- test.js
+++ test.js
@@ -5,5 +5,11 @@ Make one big string with all greetings.
 The solution should be one string with a comma in between every "Hello (Name)".
 */
 function greetPeople(names) {
+  names.forEach((name, index) => names[index] = `Hello ${name}`);
+  console.log(names);
 
+  names.toString();
+  console.log(names);
 }
+
+greetPeople(["Ironman", "Superman", "Antman", "Aquaman"]);
