fd3093f1952b323cb1c287f2a6e47ee50cea119d
Kara Bayse <karabayse@gmail.com>
2020-05-01 16:46:43 -0500

    half, quarter, and eighth

--- edabit.js
+++ edabit.js
@@ -1909,5 +1909,14 @@ greetPeople(["Ironman", "Superman", "Antman", "Aquaman"]);
 // Create a function that takes a number and return an array of three numbers:
 // half of the number, quarter of the number and an eighth of the number.
 const halfQuarterEighth = (n) => {
+  let numArr = [];
 
+  let half = n / 2;
+  let quarter = n / 4;
+  let eighth = n / 8;
+
+  numArr.push(half, quarter, eighth);
+  console.log(numArr);
 }
+
+halfQuarterEighth(64);
--- test.js
+++ test.js
@@ -1,5 +1,14 @@
 // Create a function that takes a number and return an array of three numbers:
 // half of the number, quarter of the number and an eighth of the number.
 const halfQuarterEighth = (n) => {
+  let numArr = [];
 
+  let half = n / 2;
+  let quarter = n / 4;
+  let eighth = n / 8;
+
+  numArr.push(half, quarter, eighth);
+  console.log(numArr);
 }
+
+halfQuarterEighth(64);
