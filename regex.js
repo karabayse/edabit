0f7b4de6f0eb5c9dbaeda919e7bacd6ca8267ac0
Kara Bayse <karabayse@gmail.com>
2020-05-04 19:19:54 -0500

    regex special characters

--- regex.js
+++ regex.js
@@ -1,17 +1,3 @@
-/*
-Using the .test() method in your function, return whether a string contains the
-characters "a" and "c" (in that order) with any number of characters (including
-zero) between them.
-*/
-function asterisk(string) {
-  let ac = new RegExp(\\b([ac])\\1\\1+\\b);
-  let res = ac.test(string);
-  console.log(res);
-}
-
-asterisk("Yes");
-
-/*
 \\b([a-zA-Z0-9])\\1\\1+\\b
 
 explanation:
@@ -23,4 +9,3 @@ explanation:
   \\1             : same character as group 1
   \\1+            : same character as group 1 one or more times
 \\b               : zero-length word boundary
-*/
