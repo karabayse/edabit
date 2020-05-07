d364cab2ee889baa68a52070f095886e7548741f
Kara Bayse <karabayse@gmail.com>
2020-05-06 19:38:01 -0500

    product of all odd integers

--- edabit.js
+++ edabit.js
@@ -1935,5 +1935,15 @@ function asterisk(string) {
 
 // Create a function that returns the product of all odd integers in an array.
 function oddProduct(arr) {
+  oddArr = [];
 
+  for (var i = 0; i < arr.length; i++) {
+    if (i % 2 !== 0) {
+      oddArr.push(i);
+    }
+    let product = oddArr.reduce( (a,b) => a * b, 0 ); 
+    console.log(product);
+  }
 }
+
+oddProduct([1,2,3]);
--- test.js
+++ test.js
@@ -1,26 +1,14 @@
-/*
-Using the .test() method in your function, return whether a string contains the
-characters "a" and "c" (in that order) with any number of characters (including
-zero) between them.
-*/
-function asterisk(string) {
-  let ac = new RegExp(\\b([ac])\\1\\1+\\b);
-  let res = ac.test(string);
-  console.log(res);
+// Create a function that returns the product of all odd integers in an array.
+function oddProduct(arr) {
+  oddArr = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (i % 2 !== 0) {
+      oddArr.push(i);
+    }
+    let product = oddArr.reduce( (a,b) => a * b, 0);
+    console.log(product);
+  }
 }
 
-asterisk("Yes");
-
-/*
-\\b([a-zA-Z0-9])\\1\\1+\\b
-
-explanation:
-
-\\b               : zero-length word boundary
-  (               : start capture group 1
-    [a-zA-Z0-9]   : a letter or a digit
-  )               : end group
-  \\1             : same character as group 1
-  \\1+            : same character as group 1 one or more times
-\\b               : zero-length word boundary
-*/
+oddProduct([1,2,3]);
