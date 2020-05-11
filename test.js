99aae7f3d0f253b5be552a140fec080907c5c2e4
Kara Bayse <karabayse@gmail.com>
2020-05-10 19:42:53 -0500

    x and y coordinates

--- test.js
+++ test.js
@@ -4,11 +4,11 @@
 // ➞ [[9, 1], [8, 1], [3, 1]]
 function convertCartesian(x, y) {
   // loop through two arrays and create new arrays
-  // Use the second parameter forEach accepts instead, which will be the current
-  // index you're iterating over:
-  x.forEach((num1, index) => {
-  const num2 = y[index];
-  console.log(num1, num2);
+  for (var i = 0; i < x.length; i++) {
+    let x0 = [];
+    x0.push(i);
+    // forEach loop ?
+  }
 });
   // new arrays:  [position 0, position 0], [position 1, position 1], etc.
 }
