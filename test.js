65c0820c5f1ec3621389beef6d79f5e659d7f165
Kara Bayse <karabayse@gmail.com>
2020-05-20 20:53:23 -0500

    typing game

--- test.js
+++ test.js
@@ -1,15 +1,18 @@
 function correctStream(user, correct) {
 
   // Get the value type
-  	var type = Object.prototype.toString.call(value);
+	var type = Object.prototype.toString.call(value);
 
-  	// If the two objects are not the same type, return false
-  	if (type !== Object.prototype.toString.call(other)) return false;
+	// If the two objects are not the same type, return false
+	if (type !== Object.prototype.toString.call(other)) return false;
 
-  	// More tests will go here...
+	// If items are not an object or array, return false
+	if (['[object Array]', '[object Object]'].indexOf(type) < 0) return false;
 
-  	// If nothing failed, return true
-  	return true;
+	// More tests will go here...
+
+	// If nothing failed, return true
+	return true;
 }
 
 correctStream(["cat", "blue", "skt", "umbrells", "paddy"], ["cat", "blue", "sky", "umbrella", "paddy"]);
