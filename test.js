f77a6429c61ea3ab2cefc55ce283f4a95f82c518
Kara Bayse <karabayse@gmail.com>
2020-05-23 19:43:36 -0500

    maximum edge of a triangle

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 // Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
 function nextEdge(side1, side2) {
-
+  var s = (side1 + side2 + side3)/2;
+  var area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
 }
