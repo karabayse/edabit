249b81a4ead1a1565ffd8a6933c29ef40aded5fc
Kara Bayse <karabayse@gmail.com>
2020-05-26 18:46:34 -0500

    maximum edge of a triangle

--- edabit.js
+++ edabit.js
@@ -1980,7 +1980,11 @@ function correctStream(user, correct) {
 correctStream(["cat", "blue", "skt", "umbrells", "paddy"], ["cat", "blue", "sky", "umbrella", "paddy"]);
 
 
-// Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+// Create a function that finds the maximum range of a triangle's third edge,
+// where the side lengths are all integers.
+// The sum of two sides must always be greater than the third side
 function nextEdge(side1, side2) {
-
+  let sum = side1 + side2;
+  let max = sum - 1;
+  return max;
 }
--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-// Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+// Create a function that finds the maximum range of a triangle's third edge,
+// where the side lengths are all integers.
+// The sum of two sides must always be greater than the third side
 function nextEdge(side1, side2) {
-  var s = (side1 + side2 + side3)/2;
-  var area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+  let sum = side1 + side2;
+  let max = sum - 1;
+  return max;
 }
