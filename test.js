7c0310504c6fd2251372ac1059aa63e40701aae1
Kara Bayse <karabayse@gmail.com>
2020-05-29 18:31:30 -0500

    minimum removals to make sum even

--- test.js
+++ test.js
@@ -3,5 +3,14 @@
 // If the sum is already even, return 0  ->  minimumRemovals([5, 7, 9, 11]) ➞ 0
 // The output will be either 0 or 1
 function minimumRemovals(arr) {
-
+  // add all numbers
+  let sum = arr.reduce(function(a, b){
+    console.log(a + b);
+  }, 0);
+  // determine odd or even
+  if (sum % 2 == 0) {
+    console.log(0);
+  } else if (sum % 2 !== 0) {
+    // code to remove numbers 
+  }
 }
