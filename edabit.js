21cb887622a88d7a7628314cfbbf26667080df7d
Kara Bayse <karabayse@gmail.com>
2020-05-28 19:54:12 -0500

    minimum removals to make sum even

--- edabit.js
+++ edabit.js
@@ -1992,6 +1992,8 @@ function nextEdge(side1, side2) {
 
 // Create a function that returns the minimum number of removals to make the sum
 // of all elements in an array even.
+// If the sum is already even, return 0  ->  minimumRemovals([5, 7, 9, 11]) ➞ 0
+// The output will be either 0 or 1
 function minimumRemovals(arr) {
 
 }
