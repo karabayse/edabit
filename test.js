aeaf037d303627129ad25a444a49fd49d50c78f0
Kara Bayse <karabayse@gmail.com>
2020-06-06 19:19:43 -0500

    regex character classes ix - \w

--- test.js
+++ test.js
@@ -3,13 +3,12 @@ You can think of character classes as characters with special meaning.
 They are recognized as special when you place the \ before the character.
 
 Here is a list of the character classes in JavaScript:
-
 ., \cX, \d, \D, \f, \n, \r, \s, \S, \t, \v, \w, \W, \0, \xhh, \uhhhh, \uhhhhh, [\b]
-There is a hidden word in this string:
 
+There is a hidden word in this string:
 const str = "**^&$Regular#$%Expressions$%$$%^**"
 Write the regular expression that reveals the hidden word.
 You have to remove all of the special characters to reveal the word.
 Use the character class \w in your expression.
 */
-const REGEXP = /* Write you expression here */g
+const REGEXP = /* Write your expression here */g
