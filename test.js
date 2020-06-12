91f8dccc9309de4da15f2865660b57c3f950c016
Kara Bayse <karabayse@gmail.com>
2020-06-11 19:14:22 -0500

    ignore first and last array elements

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 // Given a sandwich (as an array), return an array of fillings inside the sandwich.
 // This involves ignoring the first and last elements.
 function getFillings(sandwich) {
-
+  sandwich.shift();
+  sandwich.pop();
+  console.log(sandwich);
 }
+
+getFillings(["bread", "lettuce", "bacon", "tomato", "bread"]);
