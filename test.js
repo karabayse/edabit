1022bc2e02ed539cdcfe83314db1668e0f934f44
Kara Bayse <karabayse@gmail.com>
2020-06-18 17:36:10 -0500

    mirror array

--- test.js
+++ test.js
@@ -2,16 +2,23 @@
 // Do not repeat the last item of the given array.
 // mirror([0, 2, 4, 6]) ➞ [0, 2, 4, 6, 4, 2, 0]
 function mirror(arr) {
+  let copyArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    copyArr.push(arr[i]);
+    console.log(copyArr);
+  }
+
   let revArr = arr.reverse();
   console.log(revArr); // [6, 4, 2, 0]
 
   revArr.shift();
   console.log(revArr); // [4, 2, 0]
 
-  console.log(arr); // [4, 2, 0]  ->  reverse changes the original array 
-  arr.push(revArr);
+  for (var i = 0; i < revArr.length; i++) {
+    copyArr.push(arr[i]);
+  }
 
-  console.log(arr);
+  console.log(copyArr);
 }
 
 mirror([0, 2, 4, 6]);
