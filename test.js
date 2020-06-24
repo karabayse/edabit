5d0847cf431a2abb6358822cd73891bfc11b6903
Kara Bayse <karabayse@gmail.com>
2020-06-23 19:04:32 -0500

    video streaming plans

--- test.js
+++ test.js
@@ -22,3 +22,22 @@ class BasicPlan {
 }
 
 // Write the classes for StandardPlan and PremiumPlan here!
+class StandardPlan {
+	static canStream = true;
+	static canDownload = true;
+	static numOfDevices = 2;
+	static hasSD = true;
+	static hasHD = true;
+	static hasUHD = false;
+	static price = '$12.99';
+}
+
+class PremiumPlan {
+	static canStream = true;
+	static canDownload = true;
+	static numOfDevices = 4;
+	static hasSD = true;
+	static hasHD = true;
+	static hasUHD = true;
+	static price = '$15.99';
+}
