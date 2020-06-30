33e08f3f0a5ac873886bbb8ce87f4efb975a956c
Kara Bayse <karabayse@gmail.com>
2020-06-30 16:35:02 -0500

    product divisible by sum

--- edabit.js
+++ edabit.js
@@ -2180,5 +2180,26 @@ class PremiumPlan extends BasicPlan {
 // Write a function that returns true if the product of an array is divisible by
 // the sum of that same array. Otherwise, return false.
 function divisible(arr) {
+  let productArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    productArr.push(arr[i] * i);
+  }
+  let sumArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    sumArr.push(arr[i] + i);
+  }
 
+  console.log(productArr);
+  console.log(sumArr);
+
+  if (productArr % sumArr == 0) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
 }
+
+divisible([4, 2, 6]) // ➞ true
+// 4 * 2 * 6 / 4 + 2 + 6
+
+divisible([3, 5, 1]) //➞ false
--- test.js
+++ test.js
@@ -1,18 +1,10 @@
 // Write a function that returns true if the product of an array is divisible by
 // the sum of that same array. Otherwise, return false.
 function divisible(arr) {
-  let productArr = [];
-  for (var i = 0; i < arr.length; i++) {
-    productArr.push(arr[i] * i);
-  }
-  let sumArr = [];
-  for (var i = 0; i < arr.length; i++) {
-    sumArr.push(arr[i] + i);
-  }
+  let duplicateArr = arr.slice();
+  let productArr = arr.reduce( (a,b) => a * b, 0 );
+  let sumArr = duplicateArr.reduce( (a,b) => a + b, 0 );
 
-  console.log(productArr);
-  console.log(sumArr);
-  
   if (productArr % sumArr == 0) {
     console.log(true);
   } else {
@@ -20,7 +12,6 @@ function divisible(arr) {
   }
 }
 
-divisible([4, 2, 6]) // ➞ true
-// 4 * 2 * 6 / 4 + 2 + 6
+//divisible([4, 2, 6]) // ➞ true
 
 divisible([3, 5, 1]) //➞ false
