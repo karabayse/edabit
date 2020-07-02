85578f0a6156cb31c7ff4c48d504feded3b511c1
Kara Bayse <karabayse@gmail.com>
2020-07-01 19:22:30 -0500

    product divisible by sum

--- test.js
+++ test.js
@@ -2,16 +2,16 @@
 // the sum of that same array. Otherwise, return false.
 function divisible(arr) {
   let duplicateArr = arr.slice();
-  let productArr = arr.reduce( (a,b) => a * b, 0 );
-  let sumArr = duplicateArr.reduce( (a,b) => a + b, 0 );
+  let productArr = arr.reduce( (a,b) => a * b);
+  let sumArr = duplicateArr.reduce( (a,b) => a + b);
 
-  if (productArr % sumArr == 0) {
+  if (productArr % sumArr === 0) {
     console.log(true);
   } else {
     console.log(false);
   }
 }
 
-//divisible([4, 2, 6]) // ➞ true
+divisible([4, 2, 6]) // ➞ true
 
 divisible([3, 5, 1]) //➞ false
