95ced7cc3af8f6085952f979450ad96edf349709
Kara Bayse <karabayse@gmail.com>
2020-07-03 12:46:23 -0500

    smash factor

--- edabit.js
+++ edabit.js
@@ -2212,5 +2212,8 @@ smashFactor(181.2, 124.5) ➞ 1.46
 smashFactor(154.7, 104.3) ➞ 1.48
 */
 function smashFactor(bs, cs) {
-
+  let formula = Math.round(bs / cs) / 100;
+  console.log(formula);
 }
+
+smashFactor(139.4, 93.8);
