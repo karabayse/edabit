d1b3e63e5a72a0c55f1971e4fe0570a000ff7116
Kara Bayse <karabayse@gmail.com>
2020-07-05 19:57:11 -0500

    format text

--- edabit.js
+++ edabit.js
@@ -2203,13 +2203,6 @@ The formula for calculating smash factor is ball speed divided by club speed.
 
 Create a function that takes ball speed bs and club speed cs as arguments and
 returns the smash factor to the nearest hundredth.
-
-Examples
-smashFactor(139.4, 93.8) ➞ 1.49
-
-smashFactor(181.2, 124.5) ➞ 1.46
-
-smashFactor(154.7, 104.3) ➞ 1.48
 */
 function smashFactor(bs, cs) {
   let sFHundred = Math.round((bs / cs) * 100) / 100;
@@ -2217,3 +2210,31 @@ function smashFactor(bs, cs) {
 }
 
 smashFactor(139.4, 93.8);
+
+
+/*
+In this challenge, you have to format a word using four specific methods of the
+Markdown language that is used by Edabit to format the text in the Comments tab
+and the Instructions tab (during the creation, or the translation, of a challenge).
+Each of these four methods (or styles) is identified by the lowercased initial
+letter of its name:
+
+"b" is for bold
+"i" is for italics
+"c" is for inline code
+"s" is for strikethrough
+You are given two parameters: a string word being the word to format, and
+another string style being the lowercased initial of the style to apply.
+You have to implement a function that returns a string being the word surrounded
+by the special characters used to apply the given style.
+
+Examples
+To correctly apply a format, the word has to both start and end with one or two
+specific special characters. There are no spaces between the characters and the
+word. You can find the characters to use for this challenge and how to return
+the result in the Comments tab! Just click on the Formatting Help link that you
+can find in the Comments tab of this challenge:
+*/
+function mdFormat(word, style) {
+
+}
