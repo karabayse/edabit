a9519f66b7932fee8f889266d0337a1d044e6bd9
Kara Bayse <karabayse@gmail.com>
2020-07-08 20:10:51 -0500

    count the capital letters

--- edabit.js
+++ edabit.js
@@ -2248,5 +2248,6 @@ mdFormat("word", "s");
 
 // Given a string of letters, how many capital letters are there?
 function capitalLetters(str) {
-
+  let numUpper = str.length - str.replace(/[A-Z]/g, '').length;
+  console.log(numUpper);
 }
--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 // Given a string of letters, how many capital letters are there?
 function capitalLetters(str) {
-
+  let numUpper = str.length - str.replace(/[A-Z]/g, '').length;
+  console.log(numUpper);
 }
