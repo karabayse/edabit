584fdff291c7968ad4d9213d8a10a9f6340644f7
Kara Bayse <karabayse@gmail.com>
2020-07-12 18:30:48 -0500

    return only the integers

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
 // Write a function that takes an array of elements and returns only the integers.
 function returnOnlyInteger(arr) {
-
+  let intArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (typeof arr[i] == Number) {
+      intArr.push(arr[i]);
+    }
+  }
+  console.log(intArr);
 }
+
+returnOnlyInteger([a, b, c, 1, 2, 3]);
