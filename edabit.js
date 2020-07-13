3cddcad4ecf3574c09a5119dee0c7726f0de28a1
Kara Bayse <karabayse@gmail.com>
2020-07-13 17:03:29 -0500

    return only the integers

--- edabit.js
+++ edabit.js
@@ -2255,5 +2255,15 @@ function capitalLetters(str) {
 
 // Write a function that takes an array of elements and returns only the integers.
 function returnOnlyInteger(arr) {
-
+  let intArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (typeof(arr[i]) == "number") {
+      if (arr[i] % 1 == 0) {
+        intArr.push(arr[i]);
+      }
+    }
+  }
+  console.log(intArr);
 }
+
+returnOnlyInteger([a, b, c, 1, 2, 3]);
