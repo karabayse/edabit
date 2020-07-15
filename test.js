488a4cf58792b1256cbbd031669ccf15658c1022
Kara Bayse <karabayse@gmail.com>
2020-07-15 16:23:03 -0500

    repeating letters n times

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 // Create a function that repeats each character in a string n times
 function repeat(str, n) {
-  let strArr = str.split();
+  let strArr = str.split("");
+  console.log(strArr);
   let newArr = [];
   for (let i = 0; i < strArr.length; i++) {
       let repChar = strArr[i].repeat(n);
       newArr.push(repChar);
   }
-  return newArr;
+  console.log(newArr);
 }
+
+repeat("mice");
