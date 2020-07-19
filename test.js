d3416168d5a859a5bd25aa3a3ee0ba8394749e6a
Kara Bayse <karabayse@gmail.com>
2020-07-19 15:58:09 -0500

    characters in shapes

--- test.js
+++ test.js
@@ -1,5 +1,26 @@
 // Create a function that counts how many characters make up a rectangular shape.
 // You will be given a array of strings.
+// Return 0 if given an empty array.
+/*
+countCharacters([
+  "###",
+  "###",
+  "###"
+]) ➞ 9
+
+countCharacters([
+  "22222222",
+  "22222222",
+]) ➞ 16
+
+countCharacters([
+  "------------------"
+]) ➞ 18
+
+countCharacters([]) ➞ 0
+
+countCharacters([ "", ""]) ➞ 0
+*/
 function countCharacters(arr) {
 
 }
