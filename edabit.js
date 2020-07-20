918be2170c9e79338109007cf147e9dea5788e6c
Kara Bayse <karabayse@gmail.com>
2020-07-20 14:15:50 -0500

    characters in shapes

--- edabit.js
+++ edabit.js
@@ -2289,6 +2289,26 @@ repeat("mice", 3);
 
 // Create a function that counts how many characters make up a rectangular shape.
 // You will be given a array of strings.
+/*
+countCharacters([
+  "###",
+  "###",
+  "###"
+]) ➞ 9
+
+countCharacters([
+  "22222222",
+  "22222222",
+]) ➞ 16
+
+countCharacters([
+  "------------------"
+]) ➞ 18
+
+countCharacters([]) ➞ 0
+
+countCharacters([ "", ""]) ➞ 0
+*/
 function countCharacters(arr) {
 
 }
