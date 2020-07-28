daf9171f01fd4336acb98f405b4c51df79581019
Kara Bayse <karabayse@gmail.com>
2020-07-27 19:24:14 -0500

    return something to me

--- edabit.js
+++ edabit.js
@@ -2336,5 +2336,7 @@ countCharacters([
 // Write a function that returns the string "something" joined with a space and
 // the given parameter a.
 function giveMeSomething(a) {
-
+  // insert "something" in front of a
+  let something = "something".concat(' ', a);
+  return something;
 }
