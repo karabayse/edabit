743a41a641e06eda30f27fb442f864e0074667fe
Kara Bayse <karabayse@gmail.com>
2020-07-29 18:09:52 -0500

    where's bob?

--- edabit.js
+++ edabit.js
@@ -2345,5 +2345,6 @@ function giveMeSomething(a) {
 // Write a function that searches an array of names (unsorted) for the name "Bob"
 // and returns the location in the array. If Bob is not in the array, return -1.
 function findBob(names) {
-
+  let location = names.indexOf("Bob");
+  return location;
 }
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 // Write a function that searches an array of names (unsorted) for the name "Bob"
 // and returns the location in the array. If Bob is not in the array, return -1.
 function findBob(names) {
-
+  let location = names.indexOf("Bob");
+  return location;
 }
