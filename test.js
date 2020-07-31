c2bf61365a479df2fa6bcffe19a6fccf9d8599c9
Kara Bayse <karabayse@gmail.com>
2020-07-31 13:28:46 -0500

    broken bridge

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 // Create a function which validates whether a bridge is safe to walk on
 // (i.e. has no gaps in it to fall through).
 function isSafeBridge(str) {
-
+  return str.indexOf('') >= 0;
 }
+
+isSafeBridge("####") ➞ true
+
+isSafeBridge("## ####") ➞ false
+
+isSafeBridge("#") ➞ true
