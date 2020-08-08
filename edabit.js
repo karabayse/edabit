fd381b1e81daa737f633a0cf3409f20d405d8196
Kara Bayse <karabayse@gmail.com>
2020-08-07 19:41:50 -0500

    x and y coordinates

--- edabit.js
+++ edabit.js
@@ -1951,8 +1951,7 @@ oddProduct([1,2,3]);
 
 // Create a function that converts two arrays of x- and y- coordinates into an
 // array of (x,y) coordinates.
-// convertCartesian([9, 8, 3], [1, 1, 1])
-// ➞ [[9, 1], [8, 1], [3, 1]]
+// convertCartesian([9, 8, 3], [1, 1, 1]) ➞ [[9, 1], [8, 1], [3, 1]]
 function convertCartesian(x, y) {
   // loop through two arrays and create new arrays
   // new arrays:  [position 0, position 0], [position 1, position 1], etc.
--- test.js
+++ test.js
@@ -1,14 +1,13 @@
 // Create a function that converts two arrays of x- and y- coordinates into an
 // array of (x,y) coordinates.
-// convertCartesian([9, 8, 3], [1, 1, 1])
-// ➞ [[9, 1], [8, 1], [3, 1]]
+// convertCartesian([9, 8, 3], [1, 1, 1]) ➞ [[9, 1], [8, 1], [3, 1]]
 function convertCartesian(x, y) {
   // loop through two arrays and create new arrays
   // new arrays:  [position 0, position 0], [position 1, position 1], etc.
   x.forEach((num1, index) => {
     const num2 = y[index];
     console.log(num1, num2);
-});
+  });
 }
 
 convertCartesian([9, 8, 3], [1, 1, 1]);
