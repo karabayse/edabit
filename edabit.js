3238f24de40edc4bbcba55809a190cbeadf249e1
Kara Bayse <karabayse@gmail.com>
2020-08-22 08:37:41 -0500

    promises - the reject callback

--- edabit.js
+++ edabit.js
@@ -2416,3 +2416,19 @@ function evenOddPartition(arr) {
 }
 
 evenOddPartition([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+
+// Reject the promise with the simple string "It's not a dog!".
+let promise = new Promise( (resolve) => {
+	let animal = "cat"
+  setTimeout(() => {
+		if(animal === "dog") {
+			resolve("It's a dog!")
+		}
+	  if(animal !== "dog") {
+			/* need something here, you might also need to pass
+			something else besides the resolve callback */
+      promise.reject(new Error("It's not a dog")).then(resolved, rejected);
+		}
+  }, 1000)
+})
--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-et promise = new Promise( (resolve) => {
+// Reject the promise with the simple string "It's not a dog!".
+let promise = new Promise( (resolve) => {
 	let animal = "cat"
   setTimeout(() => {
 		if(animal === "dog") {
@@ -7,6 +8,7 @@ et promise = new Promise( (resolve) => {
 	  if(animal !== "dog") {
 			/* need something here, you might also need to pass
 			something else besides the resolve callback */
+      promise.reject(new Error("It's not a dog")).then(resolved, rejected);
 		}
   }, 1000)
 })
