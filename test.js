ac5488b4804d597ec242b109f11af889174a990c
Kara Bayse <karabayse@gmail.com>
2020-08-24 16:21:20 -0500

    applying discounts

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 // Create a function that applies a discount d to every number in the array.
+// The discount is the percentage of the original price (i.e the discount of
+// "75%" to 12 would be 9 as opposed to taking off 75% (making 3)).
+// There won't be any awkward decimal numbers, only 0.5 to deal with.
 function getDiscounts(nums, d) {
-
+  for (var i = 0; i < nums.length; i++) {
+    nums[i]
+  }
 }
