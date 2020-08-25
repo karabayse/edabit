dfc9df0df505a61ef3cfb6fa50298cd5544ee64b
Kara Bayse <karabayse@gmail.com>
2020-08-25 16:25:16 -0500

    applying discounts

--- test.js
+++ test.js
@@ -2,8 +2,12 @@
 // The discount is the percentage of the original price (i.e the discount of
 // "75%" to 12 would be 9 as opposed to taking off 75% (making 3)).
 // There won't be any awkward decimal numbers, only 0.5 to deal with.
+// getDiscounts([100], "45%") ➞ [45]
 function getDiscounts(nums, d) {
+  let discount = .10 * d;
+  let discountNum;
   for (var i = 0; i < nums.length; i++) {
-    nums[i]
+    let discountNum = nums[i] * discount;
   }
+  return discountNum;
 }
