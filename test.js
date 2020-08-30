c99a286450210393a4831a6fef306eecb8681ae4
Kara Bayse <karabayse@gmail.com>
2020-08-29 19:46:14 -0500

    product of odd integers

--- test.js
+++ test.js
@@ -1,13 +1,15 @@
 // Create a function that returns the product of all odd integers in an array.
 function oddProduct(arr) {
+
   let oddArr = [];
+
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] % 2 !== 0) {
       oddArr.push(arr[i]);
-      i *= i;
-      console.log(i);
     }
-
+  }
+  for (var i = 0; i < oddArr.length; i++) {
+    i *= i;
   }
   console.log(oddArr);
 }
