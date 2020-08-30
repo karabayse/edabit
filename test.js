aded4e310164a51fc45e05d9f7ff7b3c7ff84764
Kara Bayse <karabayse@gmail.com>
2020-08-29 19:41:56 -0500

    product of odd integers

--- test.js
+++ test.js
@@ -4,8 +4,12 @@ function oddProduct(arr) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] % 2 !== 0) {
       oddArr.push(arr[i]);
-      oddArr[i] *= i;
+      i *= i;
+      console.log(i);
     }
+
   }
-  return oddArr;
+  console.log(oddArr);
 }
+
+oddProduct([1, 2, 3, 4, 5]); // 15
