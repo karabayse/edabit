f91b2cf4205defd247b4f65f406347ec6df6b66f
Kara Bayse <karabayse@gmail.com>
2020-09-21 15:52:59 -0500

    x and y coordinates

--- test.js
+++ test.js
@@ -3,9 +3,9 @@
 // convertCartesian([9, 8, 3], [1, 1, 1]) ➞ [[9, 1], [8, 1], [3, 1]]
 function convertCartesian(x, y) {
   // loop through two arrays and create new arrays
-
-  // fill()
-
+  x.map(function(index){
+   return y[index]
+ })
 }
 
 convertCartesian([9, 8, 3], [1, 1, 1]);
