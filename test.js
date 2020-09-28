9d4508547c3705c52d94c15b398ec71fa058bbf5
Kara Bayse <karabayse@gmail.com>
2020-09-27 19:04:27 -0500

    calculate using string operation

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 // Create a function that takes two numbers and a mathematical operator and returns the result.
+/*
+Numbers can be negative.
+The only operations used are those in the examples above.
+*/
 function calculate(num1, num2, op) {
-  let result = num1 op num2;
+  let result = eval(num1 operator num2);
   return result;
 }
