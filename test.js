eeffb8d9e7f105265e539477a1961da7a8968e41
Kara Bayse <karabayse@gmail.com>
2020-09-28 19:36:54 -0500

    calculate using string operation

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
 // Create a function that takes two numbers and a mathematical operator and returns the result.
 /*
 Numbers can be negative.
+
+calculate(4, 9, "+") ➞ 13
+
+calculate(12, 5, "-") ➞ 7
+
+calculate(6, 3, "*") ➞ 18
+
+calculate(25, 5, "/") ➞ 5
+
+calculate(14, 3, "%") ➞ 2
+
 The only operations used are those in the examples above.
 */
 function calculate(num1, num2, op) {
