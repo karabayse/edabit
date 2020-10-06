b7a805d41333adce1ec47826118cb58db275854b
Kara Bayse <karabayse@gmail.com>
2020-10-06 18:33:36 -0500

    total number of unique characters

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 // Given two strings, create a function that returns the total number of unique
 // characters from the combined string.
 function countUnique(s1, s2) {
+  let comboString = s1.concat(s2);
+  let strArr = comboString.split(' ');
+  console.log(strArr);
 
 }
+
+countUnique("apple", "play");
