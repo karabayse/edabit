4b7c16bba3b2311c844fd9147347e94f87d41201
Kara Bayse <karabayse@gmail.com>
2020-10-09 18:45:28 -0500

    hamming distance

--- test.js
+++ test.js
@@ -2,7 +2,11 @@
 // Hamming distance is the number of characters that differ between two strings.
 // Both strings will have the same length.
 function hammingDistance(str1, str2) {
-
+  for (let i = 0; i < str1.length; i++) {
+            if (str1++[++i].toLowerCase() != str2++[++i].toLowerCase()) {
+                result++
+            }
+        }
 }
 
 hammingDistance("abcde", "bcdef") // ➞ 5
