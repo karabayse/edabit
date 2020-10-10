f649398bf12dea01ab20292c6f3f55a8561e67f3
Kara Bayse <karabayse@gmail.com>
2020-10-10 18:08:42 -0500

    hamming distance

--- edabit.js
+++ edabit.js
@@ -2497,3 +2497,18 @@ function countUnique(s1, s2) {
 }
 
 countUnique("apple", "play");
+
+
+// Create a function that computes the hamming distance between two strings.
+// Hamming distance is the number of characters that differ between two strings.
+// Both strings will have the same length.
+function hammingDistance(str1, str2) {
+  for (let i = 0; i < str1.length; i++) {
+    if (str1++[++i].toLowerCase() != str2++[++i].toLowerCase()) {
+        result++
+    }
+  }
+    return result;
+}
+
+hammingDistance("abcde", "bcdef") // ➞ 5
--- test.js
+++ test.js
@@ -3,10 +3,11 @@
 // Both strings will have the same length.
 function hammingDistance(str1, str2) {
   for (let i = 0; i < str1.length; i++) {
-            if (str1++[++i].toLowerCase() != str2++[++i].toLowerCase()) {
-                result++
-            }
-        }
+    if (str1++[++i].toLowerCase() != str2++[++i].toLowerCase()) {
+        result++
+    }
+  }
+    return result;
 }
 
 hammingDistance("abcde", "bcdef") // ➞ 5
