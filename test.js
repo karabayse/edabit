e7c6c21dfc341b0b33ecbeeecf364b60453980f6
Kara Bayse <karabayse@gmail.com>
2020-10-21 19:04:08 -0500

    character in shapes

--- test.js
+++ test.js
@@ -2,5 +2,8 @@
 // make up the shape. You will be given an array of strings which make up a shape
 // in the compiler (i.e. a square, a rectangle or a line).
 function countCharacters(arr) {
-
+  let arrLength = arr.length;
+  let strLength = arr[0].length;
+  let characters = arrLength * strLength;
+  return characters;
 }
