251bfff7942db96a0e810c8b7923880a96f7a953
Kara Bayse <karabayse@gmail.com>
2020-10-23 19:34:22 -0500

    character in shapes

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
 // Create a function to calculate how many characters in total are needed to
 // make up the shape. You will be given an array of strings which make up a shape
 // in the compiler (i.e. a square, a rectangle or a line).
+// Return 0 if the given array is empty.
 function countCharacters(arr) {
   let arrLength = arr.length;
   console.log(arrLength);
+
+  if (arrLength == 0) {
+    return 0;
+  }
 
   let strLength = arr[0].length;
   console.log(strLength);
@@ -19,3 +24,7 @@ countCharacters([
   "###",
   "###"
 ]) // ➞ 9
+
+countCharacters([
+  "------------------"
+]) // ➞ 18
