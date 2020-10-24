858d619e4288f31fcc1d7d10604ba8342b108973
Kara Bayse <karabayse@gmail.com>
2020-10-23 19:35:56 -0500

    character in shapes

--- edabit.js
+++ edabit.js
@@ -2546,6 +2546,23 @@ cubeSquareRoot(81) // ➞ 729
 // Create a function to calculate how many characters in total are needed to
 // make up the shape. You will be given an array of strings which make up a shape
 // in the compiler (i.e. a square, a rectangle or a line).
+// Return 0 if the given array is empty.
 function countCharacters(arr) {
-
+  let arrLength = arr.length;
+  if (arrLength == 0) {
+    return 0;
+  }
+  let strLength = arr[0].length;
+  let characters = arrLength * strLength;
+  return characters;
 }
+
+countCharacters([
+  "###",
+  "###",
+  "###"
+]) // ➞ 9
+
+countCharacters([
+  "------------------"
+]) // ➞ 18
