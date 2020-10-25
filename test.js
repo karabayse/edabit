5de885ee1fb28ee4661329ee070abf296304452c
Kara Bayse <karabayse@gmail.com>
2020-10-25 16:22:00 -0500

    calculate the mean

--- test.js
+++ test.js
@@ -2,5 +2,16 @@
 // dividing this sum by the total count of numbers in the group. Given a sorted
 // array of numbers, return the mean (rounded to one decimal place).
 function mean(nums) {
+   let numsSum = nums.reduce((a, b) => a + b, 0);
+   console.log(numsSum);
 
+   let numsNum = nums.length;
+
+   let numsMean = numsSum / numsNum;
+   console.log(numsMean);
+
+   let numsRounded = numsMean.toFixed(1);
+   console.log(numsRounded);
 }
+
+mean([1, 3, 8, 9, 9, 10]) // ➞ 6.7
