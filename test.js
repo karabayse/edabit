8d1418f4560e830d32bb64dc4f75d560d3615d4a
Kara Bayse <karabayse@gmail.com>
2020-11-01 10:01:25 -0600

    give me the even numbers

--- test.js
+++ test.js
@@ -3,13 +3,14 @@
 // Remember that the start and stop values are inclusive.
 function sumEvenNumsInRange(start, stop) {
   let i;
-  for (let i = start; i <= stop; i++) {
-    if (start % 2 == 0) {
-      i += 2;
-      i += i;
+  let sum;
+  if (start % 2 == 0) {
+    for (let i = start; i <= stop; i += 2) {
+      sum += i;
+      console.log(i);
     }
-    console.log(i);
   }
+  console.log(sum);
 }
 
 sumEvenNumsInRange(51, 150) // ➞ 5050
