cd957cf3c849df12f58869ede1ab547797dec602
Kara Bayse <karabayse@gmail.com>
2020-11-07 08:28:52 -0600

    owofied a sentence

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 // Create a function that takes a sentence and turns every "i" into "wi" and "e"
 // into "we", and add "owo" at the end.
 function owofied(sentence) {
-
+  sentence.replace("i", "wi");
+  sentence.replace("e", "we");
+  sentence.concat(" owo");
+  console.log(sentence);
 }
 
 owofied("I'm gonna ride 'til I can't no more")
