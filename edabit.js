af81746e2950cb82df1a1485279d3ac70e8e7776
Kara Bayse <karabayse@gmail.com>
2020-11-08 14:08:04 -0600

    filter out empty arrays

--- edabit.js
+++ edabit.js
@@ -2667,3 +2667,12 @@ function removeEmptyArrays(arr) {
 
 removeEmptyArrays(["a", "b", []]) ➞ ["a", "b"];
 removeEmptyArrays([1, 2, [], 4]) ➞ [1, 2, 4];
+
+
+// Filter out empty arrays
+function removeEmptyArrays(arr) {
+  return arr.filter(x => x.length !== 0)
+}
+
+removeEmptyArrays(["a", "b", []]) // ➞ ["a", "b"];
+removeEmptyArrays([1, 2, [], 4]) // ➞ [1, 2, 4];
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // Filter out empty arrays
 function removeEmptyArrays(arr) {
-  return arr.filter(x => x !== []);
+  return arr.filter(x => x.length !== 0)
 }
 
-removeEmptyArrays(["a", "b", []]) ➞ ["a", "b"];
-removeEmptyArrays([1, 2, [], 4]) ➞ [1, 2, 4];
+removeEmptyArrays(["a", "b", []]) // ➞ ["a", "b"];
+removeEmptyArrays([1, 2, [], 4]) // ➞ [1, 2, 4];
