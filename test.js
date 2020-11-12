4ca24ecc39f7edae66923a143d040635424a242f
Kara Bayse <karabayse@gmail.com>
2020-11-12 12:34:30 -0600

    convert year to century

--- test.js
+++ test.js
@@ -1,15 +1,18 @@
 // Write a function that takes a year and returns its corresponding century.
 function centuryFromYear(year) {
   let numStr = year.toString();
+  let century;
 
-  if (year.length == 3) {
+  if (year.length < 4) {
     let first = numStr.substring(0,1);
+    let century = first + 1;
+    console.log(century);
+  } else if (year.length == 4) {
+    let firstTwo = numStr.substring(0,2);
+    let num = parseInt(firstTwo);
     let century = num + 1;
+    console.log(century);
   }
-
-  let firstTwo = numStr.substring(0,2);
-  let num = parseInt(firstTwo);
-  let century = num + 1;
   console.log(century);
 
   /*
