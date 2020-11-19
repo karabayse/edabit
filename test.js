48f97249bb64dbcec424b0634bc814c8e339d36a
Kara Bayse <karabayse@gmail.com>
2020-11-19 16:13:45 -0600

    convert year to century

--- test.js
+++ test.js
@@ -5,27 +5,27 @@ function centuryFromYear(year) {
 
   if (numStr.length == 1) {
     let century = 1;
-    return century;
+    console.log(century);
   } else if (numStr.length == 2 && numStr[1] == 0) {
     let first = numStr.substring(0,1);
     let num = parseInt(first);
     let century = num + 1;
-    return century;
+    console.log(century);
   } else if (numStr.length == 3 && numStr[-1] == 0 && numStr[-2] == 0) {
     let first = numStr.substring(0,1);
     let num = parseInt(first);
     let century = num + 1;
-    return century;
+    console.log(century);
   } else if (numStr.length < 4) {
     let first = numStr.substring(0,1);
     let num = parseInt(first);
     let century = num + 1;
-    return century;
+    console.log(century);
   } else if (numStr.length == 4) {
     let firstTwo = numStr.substring(0,2);
     let num = parseInt(firstTwo);
     let century = num + 1;
-    return century;
+    console.log(century);
   }
 }
 
@@ -34,3 +34,5 @@ centuryFromYear(2005); // ➞ 21
 centuryFromYear(950); // ➞ 10
 
 centuryFromYear(1900); // ➞ 19
+
+centuryFromYear(2);
