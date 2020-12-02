a4451c37473f1b6b16272bdfcd1793976f24da68
Kara Bayse <karabayse@gmail.com>
2020-12-01 18:05:59 -0600

    how much is true?

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
 // Create a function which returns the number of true values there are in an array.
+// Return 0 if given an empty array.
+// All array items are of the type bool (true or false).
 function countTrue(arr) {
 
 }
+
+countTrue([true, false, false, true, false]) // ➞ 2
+
+countTrue([false, false, false, false]) // ➞ 0
+
+countTrue([]) // ➞ 0
