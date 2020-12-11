f9a531492c06c6cd0c72305b51daa6a8411493e3
Kara Bayse <karabayse@gmail.com>
2020-12-11 16:15:59 -0600

    ones, threes, and nines

--- test.js
+++ test.js
@@ -3,6 +3,14 @@
 // You must create a class.
 // You will make variables (class.ones, class.threes, class.nines) to do this.
 // Use getters and setters
+class OnesThreesNines {
+  constructor(ones, threes, nines) {
+    this.ones = ones;
+    this.threes = threes;
+    this.nines = nines;
+  }
+}
+
 let n1 = new OnesThreesNines(5)
 n1.nines ➞ 0
 
