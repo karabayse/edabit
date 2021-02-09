f83b7f94ceced031b82a0daa640ed57819aeab8f
Kara Bayse <karabayse@gmail.com>
2021-02-09 16:11:12 -0600

    get middle character

--- test.js
+++ test.js
@@ -2,9 +2,16 @@
 Create a function that takes a string and returns the middle character(s).
 If the word's length is odd, return the middle character.
 If the word's length is even, return the middle two characters.
+All test cases contain a single word (as a string).
 */
 function getMiddle(str) {
+  var len = str.length;
 
+  if (len % 2 == 0) {
+    // return middle two characters
+  } else {
+    // return middle character 
+  }
 }
 
-getMiddle("Get middle character");
+getMiddle("middle");
