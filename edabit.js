9456d1824b583154dcd193c9233c33f51ce3a669
Kara Bayse <karabayse@gmail.com>
2021-02-12 18:49:40 -0600

    regex group ranges x/y

--- edabit.js
+++ edabit.js
@@ -2828,3 +2828,23 @@ function getMiddle(str) {
 }
 
 getMiddle("middle");
+
+
+/*
+Groups and ranges indicate groups and ranges of expression characters.
+The regular expression x|y matches either "x" or "y".
+
+const REGEXP = /blue|red/
+
+"red flag".match(REGEXP)  // red
+"blue flag".match(REGEXP)  // blue
+
+// Matches "blue" in "blue flag" and "red" in "red flag".
+Create a regular expression to match all red flag and blue flag in a string.
+You must use | in your expression. Flags can come in any order.
+
+Example:
+"red flag blue flag".match(REGEXP) ➞ ["red flag", "blue flag"]
+*/
+
+const REGEXP = /*  */g
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 /*
-Groups and ranges indicate groups and ranges of expression characters. The regular expression x|y matches either "x" or "y".
+Groups and ranges indicate groups and ranges of expression characters.
+The regular expression x|y matches either "x" or "y".
 
 const REGEXP = /blue|red/
 
@@ -7,7 +8,8 @@ const REGEXP = /blue|red/
 "blue flag".match(REGEXP)  // blue
 
 // Matches "blue" in "blue flag" and "red" in "red flag".
-Create a regular expression to match all red flag and blue flag in a string. You must use | in your expression. Flags can come in any order.
+Create a regular expression to match all red flag and blue flag in a string.
+You must use | in your expression. Flags can come in any order.
 
 Example:
 "red flag blue flag".match(REGEXP) ➞ ["red flag", "blue flag"]
